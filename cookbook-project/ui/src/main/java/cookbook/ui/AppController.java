377c9567660185a3be9a41036be7cb8314fa9459
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2023-09-29 14:19:43 +0200

    add changes to Appcontroller

--- cookbook-project/ui/src/main/java/cookbook/ui/AppController.java
+++ cookbook-project/ui/src/main/java/cookbook/ui/AppController.java
@@ -197,20 +197,15 @@ public class AppController {
 
   public void switchToViewRecipe(ActionEvent event) throws IOException {
 
-    
     Parent root = FXMLLoader.load(getClass().getResource("RecipeView.fxml"));
     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
-    public void removeRecipe() {
+  }
+  public void removeRecipe() {
     System.out.println("Recipe removed");
   }
-}
-
-  
-
-
 }
 
 
