de9f3d24558c6f1348b9accea5842f7fb2d899b0
Birk Strand Bjornaa <birk.bjornaa@gmail.com>
2023-10-11 11:11:24 +0200

    Made progress on tests

--- cookbook-project/ui/src/test/java/cookbook/ui/CookbookAppTest.java
+++ cookbook-project/ui/src/test/java/cookbook/ui/CookbookAppTest.java
@@ -1,12 +1,20 @@
 package cookbook.ui;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane; //newly added
 import javafx.stage.Stage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,6 +27,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.api.FxRobot; //newly added
 
 import cookbook.ui.AppController;
 
@@ -29,19 +38,24 @@ public class CookbookAppTest extends ApplicationTest {
 
     private AppController controller;
     private Parent root;
+    private Scene scene;
+
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("CookbookApp.fxml"));
-        root = fxmlLoader.load();
+        final Parent parent = fxmlLoader.load(); //endrer fra root til final Parent parent
         controller = fxmlLoader.getController();
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(parent)); // bytter ut root med parent
         stage.show();
+        //System.out.println("Startet test");
+        VBox search = (VBox) root.lookup("#recipeList");
+        //System.out.println(search.getChildren().size());
     }
 
-    public Parent getRootNode() {
-        return root;
-    }
+    // public Parent getRootNode() {
+    //     return root;
+    // }
 
     private String enterLabel = """
         E
@@ -57,28 +71,64 @@ public class CookbookAppTest extends ApplicationTest {
         }
     }
 
+    // private static Stream<String> searchTexts(){
+    //     return Stream.of(
+    //         "pasta",
+    //         "unavaliable", 
+    //         ""
+    //     );
+    // }
+
+    // private void clickButton(String id){
+    //     Node button = lookup(id).query();
+    //     clickOn(null, null, null);
+    //     if (button instanceof Button){
+    //         FxRobot fxRobot = new FxRobot();
+    //         fxRobot.clickOn(button, MouseButton.PRIMARY);
+    //     } else{
+    //         throw new IllegalArgumentException("Button not found");
+    //     }
+    // }
+
     //legger basert på individuell øving
-    //trengs kanskje ikke
 
     private String getSearchString() {
         TextField search = (TextField) root.lookup("#searchField");
         return search.getText();
     }
 
-    // private boolean searchPressed() {
-
-    // }
-
     // forsøk på tester:
 
-    @ParameterizedTest
-    @MethodSource
-    public void testSearch(String searchFieldText) {
-        //hvis søket er tomt - sjekk at viewet ikke endrer seg
-        //dersom det er står noe, og det samsvarer, sjekk at det kommer i view
-        //dersom ingen matcher - sjekk at alle blir og du får beskjed om feil
+    // @ParameterizedTest
+    // @MethodSource("searchTexts")
+    // public void testSearch(String searchFieldText) {
+    //     Scene scene = getScene();
+    //     ScrollPane scrollPane = (ScrollPane) scene.lookup("recipeList");
+    //     int initialScrollPaneSize = ((VBox)scrollPane.getContent()).getChildren().size(); 
         
+    //     if search field is empty, check that the scrollpane recipeList does not change contents
+    //     if something is written, all matching recipes search is displayed in scrollpane
+    //     if no matches, scrollpane does not change
+        
+    // }
+
+    @Test
+    public void testSearching(){
+        Scene testScene = new Scene(new Group());
+        VBox vBox = (VBox)testScene.lookup("recipeList");
+
+        searchRecipeFXRobot("pasta"); //må bytte til VBOX
+        assertEquals(1, (vBox.getChildren().size()));
     }
 
-    
+    private void searchRecipeFXRobot(String name){
+        clickOn("#searchField").write(name);
+        clickOn("#searchButton");
+    }
+
+    private void removeRecipeFXRobot(){
+        //hvordan nå removeknappen på en spesifikk oppskrift?
+    }
+//scrollpane har children, kan feks streame for å sjekke om det matcher search
+
 }
