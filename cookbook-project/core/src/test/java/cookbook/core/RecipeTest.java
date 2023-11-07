c630014d7ed6634c979e04bfb0577a676d66a6a3
hannajacobsen <hannajac@stud.ntnu.no>
2023-11-07 10:43:55 +0100

    [Test] Extended core tests
    - Extended the core tests to cover the new functionalities based on the new user stories. The test coverage (Jacoco) is now 100% for core.
    - Did also comment out the uneccesary constructors, that will be deleted and handle exceptions in the refactoring issue.
    
    Closes #40
    
    Co-authored by: Aurora Johansen

--- cookbook-project/core/src/test/java/cookbook/core/RecipeTest.java
+++ cookbook-project/core/src/test/java/cookbook/core/RecipeTest.java
@@ -1,11 +1,15 @@
 package cookbook.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -85,4 +89,78 @@ public class RecipeTest {
       });
         
     }
+
+    @Test
+    @DisplayName("Test if the setType method for recipes works properly.")
+    public void testSetType() {
+        Recipe recipe2 = new Recipe();
+        recipe2.setType("nothing");
+        recipe.setType("Dinner");
+        assertEquals("Dinner", recipe.getType());
+        assertEquals("Unknown", recipe2.getType());
+    }
+
+    @Test
+    @DisplayName("Test if the setFavorite method for recipes works properly.")
+    public void testSetFavorite() {
+        recipe.setFavorite(true);
+        assertEquals(true, recipe.isFavorite());
+    }
+
+    @Test
+    @DisplayName("Test if the setVegan method for recipes works properly.")
+    public void testSetVegan() {
+        recipe.setVegan(true);
+        assertEquals(true, recipe.isVegan());
+    }
+
+    @Test
+    @DisplayName("Test if the setLactoseFree method for recipes works properly.")
+    public void testSetLactoseFree() {
+        recipe.setLactoseFree(true);
+        assertEquals(true, recipe.isLactoseFree());
+    }
+
+    @Test
+    @DisplayName("Test if the setGlutenFree method for recipes works properly.")
+    public void testSetGlutenFree() {
+        recipe.setGlutenFree(true);
+        assertEquals(true, recipe.isGlutenFree());
+    }
+
+    @Test
+    @DisplayName("Test the empty constructor for recipes.")
+    public void testEmptyConstructor() {
+        Recipe recipe2 = new Recipe();
+        Map<String, String> ingredients = new HashMap<>();
+        assertEquals(null, recipe2.getName());
+        assertEquals(ingredients, recipe2.getIngredients());
+        assertEquals(null, recipe2.getOriginCountry());
+        assertEquals("Unknown", recipe2.getType());
+        assertEquals(null, recipe2.getDescription());
+        assertFalse(recipe2.isFavorite());
+        assertFalse(recipe2.isVegan());
+        assertFalse(recipe2.isGlutenFree());
+        assertFalse(recipe2.isLactoseFree());
+
+    }
+
+    @Test
+    @DisplayName("Test the full constructor for recipes.")
+    public void testFullConstructor() {
+        Map<String, String> ingredients = new HashMap<>();
+        ingredients.put("spaghetti", "20.0");
+        Recipe recipe2 = new Recipe("Spaghetti Carbonara", ingredients,"Italy", "Dinner", "A classic Italian pasta dish", true, true, false, false);
+        assertEquals("Spaghetti Carbonara", recipe2.getName());
+        assertEquals(ingredients, recipe2.getIngredients());
+        assertEquals("Italy", recipe2.getOriginCountry());
+        assertEquals("Dinner", recipe2.getType());
+        assertEquals("A classic Italian pasta dish", recipe2.getDescription());
+        assertTrue(recipe2.isFavorite());
+        assertTrue(recipe2.isVegan());
+        assertFalse(recipe2.isGlutenFree());
+        assertFalse(recipe2.isLactoseFree());
+    }
 }
+
+
